c4aca2b95ea7f80fac7f78d23d37a04a0746aee9
ManasiKerkar <41256416+ManasiKerkar@users.noreply.github.com>
2018-09-04 21:31:54 +0530

    Add files via upload

---
+++ Cleaning data course project.R
@@ -0,0 +1,44 @@
+x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+> y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+> subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+> 
+  > # Reading testing tables:
+  > x_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+  > y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+  > subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+  > 
+    > # Reading feature vector:
+    > features <- read.table('./data/UCI HAR Dataset/features.txt')
+    > 
+      > # Reading activity labels:
+      > activityLabels = read.table('./data/UCI HAR Dataset/activity_labels.txt')
+      > colnames(x_train) <- features[,2] 
+      > colnames(y_train) <-"activityId"
+      > colnames(subject_train) <- "subjectId"
+      > 
+        > colnames(x_test) <- features[,2] 
+        > colnames(y_test) <- "activityId"
+        > colnames(subject_test) <- "subjectId"
+        > 
+          > colnames(activityLabels) <- c('activityId','activityType')
+          > mrg_train <- cbind(y_train, subject_train, x_train)
+          > mrg_test <- cbind(y_test, subject_test, x_test)
+          > setAllInOne <- rbind(mrg_train, mrg_test)
+          > colNames <- colnames(setAllInOne)
+          > mean_and_std <- (grepl("activityId" , colNames) | 
+                               +                      grepl("subjectId" , colNames) | 
+                               +                      grepl("mean.." , colNames) | 
+                               +                      grepl("std.." , colNames) 
+                             + )
+          > setForMeanAndStd <- setAllInOne[ , mean_and_std == TRUE]
+          > 
+            > setWithActivityNames <- merge(setForMeanAndStd, activityLabels,
+                                            +                               by='activityId',
+                                            +                               all.x=TRUE)
+            > SecondTidydataSet <- aggregate(. ~subjectId + activityId, setWithActivityNames, mean)
+            > SecondTidydataSet <- SecondTidydataSet[order(SecondTidydataSet$subjectId, SecondTidydataSet$activityId),]
+            > 
+              > write.table(SecondTidydataset, "2nd Tidy Data set.txt", row.name=FALSE)
+            Error in is.data.frame(x) : object 'SecondTidydataset' not found
+            > write.table(SecondTidydataSet, "2nd Tidy Data set.txt", row.name=FALSE)
+            > 
---
+++ Readme.md
@@ -0,0 +1,5 @@
+Cleaning data course project : Contains required r script 
+
+Codebook.md : Steps used to create the script
+
+Output : Output is stored in 2nd Tidy Data set
